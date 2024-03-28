24eb60b50b06b689517370ed3c3816854e2be605
SooYoung Joo <asoostale@gmail.com>
2024-03-28 15:05:19 +0900

    sec5 << 로그인 구현

---
+++ src/main/java/com/asoostale/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,53 @@
+package com.asoostale.config;
+
+import com.asoostale.controller.model.Customer;
+import com.asoostale.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+//로그인 로직이 이부분임.
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findByEmail(username);
+        if (!customers.isEmpty()) {
+            if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+            }
+
+        } else {
+            throw new BadCredentialsException("등록된 유저가 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
